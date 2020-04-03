60743bae2aa7f5c3c8b3cfede80bfff7da87def0
yumit412 <yumit412@gmail.com>
2020-04-03 08:47:54 +0000

    20200403 - 3

--- src/machine.rs
+++ src/machine.rs
@@ -1,5 +1,58 @@
+use async_std::task;
+use async_std::task::JoinHandle;
+
 #[derive(Clone, Debug)]
 struct Machine{
     // modbus devices..
     // 
+}
+
+struct ModbusDevice {
+    name:String,
+    handle: Option<JoinHandle<()>>,
+}
+
+impl ModbusDevice {
+    pub fn new(name:String) -> Self{
+        Self{
+            name: name,
+            handle: None,
+        }
+    }
+
+    pub fn run() {
+        let handle = task::spawn(async {});
+    }
+
+    fn try_run(&self) {
+        loop{
+            // polling 
+        }
+    }
+
+    fn update_device(&mut self) {
+
+    }
+
+    pub fn stop(&self) {
+        if let Some(self.handle) = self.handle {
+            let handle = self.handle.unwrap();
+            task::block_on(handle);
+        }
+
+        
+    }
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn construct_test() {
+        let device = ModbusDevice::new("test device".to_string());
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,18 @@ mod server;
 mod machine;
 
 use server::Server;
+use async_std::task;
 
 fn main() {
-    println!("Hello, world!");
+    task::block_on(try_main());
+
+    println!("Terminated");
+}
+
+async fn try_main() {
+    let mut server = Server::new();
+    let server_handle = task::spawn(async {});
+
+    // wait for machine stopped
+    // wait for server stopped
 }
