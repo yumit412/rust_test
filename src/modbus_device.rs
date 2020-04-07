de92f78affa3076196c615db87c49e00dda51260
yumit412 <yumit412@gmail.com>
2020-04-07 08:52:44 +0000

    20200407 0552

--- src/modbus_device.rs
+++ src/modbus_device.rs
@@ -1,7 +1,34 @@
 use modbus::{Client, Coil};
 use modbus::tcp;
 
+enum IO_TYPE{
+    input,
+    output,
+    io,
+}
+
+struct IO<IO_TYPE> {
+    name: String,
+    address: u32,
+    value: u8,
+}
+
+impl IO_TYPE {
+    fn new(name: String) -> Self{
+        Self{
+            name:name,
+            address: None,
+            Value: None,
+        }
+    }
+}
+
 struct ModbusDevice {
+}
+
+fn construct_io_map {
+    let io1 = IO<IO_TYPE::input>::new("io1".to_string());
+    
 }
 
 fn test() {
